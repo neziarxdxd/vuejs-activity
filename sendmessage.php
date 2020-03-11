6d02dbd1702ae40c10f41d6a2836530313beeabc
raizengfilosofuxd <rickdavidsangalang@gmail.com>
2020-03-11 10:20:42 +0800

    added query

--- sendmessage.php
+++ sendmessage.php
@@ -7,4 +7,14 @@ echo $username;
 echo $message;
 echo $date;
 
+$sql = "INSERT INTO tbl tbl_messages VALUES ('$username', '$message', '$date')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
 ?>
