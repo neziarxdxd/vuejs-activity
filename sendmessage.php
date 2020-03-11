af3bab94d6b24e6a225bd82eb1a252df34d4ee27
raizengfilosofuxd <rickdavidsangalang@gmail.com>
2020-03-11 11:11:34 +0800

    added

--- sendmessage.php
+++ sendmessage.php
@@ -1,13 +1,14 @@
 <?php
+include("condb.php");
 $username = $_POST['username'];
 $message = $_POST['message'];
 $date = date('Y-m-d H:i:s');
-include("condb.php");
+
 echo $username;
 echo $message;
 echo $date;
 
-$sql = "INSERT INTO tbl tbl_messages VALUES ('$username', '$message', '$date')";
+$sql = "INSERT INTO tbl_messages VALUES ('$username', '$message', '$date')";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
