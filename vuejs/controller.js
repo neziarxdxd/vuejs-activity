9796251b1ec17d93dac6a0d16095d3ffc10305c3
raizengfilosofuxd <rickdavidsangalang@gmail.com>
2020-03-25 19:06:45 +0800

    fixed bugs

--- vuejs/controller.js
+++ vuejs/controller.js
@@ -1,6 +1,7 @@
 var app = new Vue({
     el:'#app',
     data:{
+    txt_Task:'',
     message: {
        head: 'TODO APPLICATION',
        
@@ -10,8 +11,25 @@ var app = new Vue({
         { text: 'Learn Vue' },
         { text: 'Build something awesome' }
       ]
+    },
+    methods:{
+        insertData:function(){
+            app.todos.push({text:this.txt_Task})
+        }
     }
+
+   
 })
+
+/**
+ methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+}) 
+ 
+ */
 
 var app2 = new Vue({
     el:'#app-4',
